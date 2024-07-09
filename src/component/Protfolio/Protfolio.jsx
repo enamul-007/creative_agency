0dff831ede8b91ae24965c49650e6d3ec1f76cbf
enamul-007 <anamulkhan523@gmail.com>
2024-07-10 01:28:47 +0600

    protfolio part add

--- src/component/Protfolio/Protfolio.jsx
+++ src/component/Protfolio/Protfolio.jsx
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import "./Protfolio.css";
 import Heading from "../../commonComponent/Heading";
 import profolio_1 from "../../assets/profolio_1.png";
+import profolio_2 from "../../assets/profolio_2.png";
+import Portfolio_3 from "../../assets/Portfolio_3.png";
+import Portfolio_4 from "../../assets/Portfolio_4.png";
+import Portfolio_5 from "../../assets/Portfolio_5.png";
+import Portfolio_6 from "../../assets/Portfolio_6.png";
+
 import { GoArrowRight } from "react-icons/go";
 
 const Protfolio = () => {
@@ -9,7 +15,66 @@ const Protfolio = () => {
   const handleclick = () => {
     setshow(!show);
   };
-  console.log(show);
+
+  const allprotfolioCard_part = [
+    {
+      id: 1,
+      profolio_1: profolio_1,
+      title: "Graphic Design",
+      button: "See Details",
+      icon: <GoArrowRight />,
+    },
+    {
+      id: 2,
+      profolio_1: profolio_2,
+      title: "Graphic Design",
+      button: "See Details",
+      icon: <GoArrowRight />,
+    },
+    {
+      id: 3,
+      profolio_1: Portfolio_3,
+      title: "Graphic Design",
+      button: "See Details",
+      icon: <GoArrowRight />,
+    },
+    {
+      id: 4,
+      profolio_1: Portfolio_4,
+      title: "Graphic Design",
+      button: "See Details",
+      icon: <GoArrowRight />,
+    },
+    {
+      id: 5,
+      profolio_1: Portfolio_5,
+      title: "Graphic Design",
+      button: "See Details",
+      icon: <GoArrowRight />,
+    },
+    {
+      id: 6,
+      profolio_1: Portfolio_6,
+      title: "Graphic Design",
+      button: "See Details",
+      icon: <GoArrowRight />,
+    },
+    {
+      id: 7,
+      profolio_1: profolio_1,
+      title: "Graphic Design",
+      button: "See Details",
+      icon: <GoArrowRight />,
+    },
+    {
+      id: 8,
+      profolio_1: profolio_2,
+      title: "Graphic Design",
+      button: "See Details",
+      icon: <GoArrowRight />,
+    },
+  ];
+
   return (
     <div>
       <div className="Protfolio_part">
@@ -21,23 +86,32 @@ const Protfolio = () => {
               description={`There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.`}
             />
           </div>
-          <div>
-            <div className="protfolioCard_part" onClick={handleclick}>
-              <picture>
-                <img src={profolio_1} alt={profolio_1} />
-              </picture>
-                 {show && (
-              <div className="profolioOverlay">
-                <div className="profolioCardOverlay">
-                  <h1 className="profolioCardOverlay_title">Graphic Design</h1>
-                  <a className="profolioCardOverlay_summry" href="#">
-                    See Details <GoArrowRight />
-                  </a>
-                </div>
+
+          <div className="allprotfolioCard_part">
+            {allprotfolioCard_part?.map((item) => (
+              <div
+                key={item.id}
+                className="protfolioCard_part"
+                
+                onClick={handleclick}
+              >
+                <picture>
+                  <img src={item.profolio_1} alt={item.profolio_1} />
+                </picture>
+                {show && (
+                  <div className="profolioOverlay">
+                    <div className="profolioCardOverlay">
+                      <h1 className="profolioCardOverlay_title">
+                        {item.title}
+                      </h1>
+                      <a className="profolioCardOverlay_summry" href="#">
+                        {item.button} <GoArrowRight />
+                      </a>
+                    </div>
+                  </div>
+                )}
               </div>
-            )}
-            </div>
-         
+            ))}
           </div>
         </div>
       </div>
